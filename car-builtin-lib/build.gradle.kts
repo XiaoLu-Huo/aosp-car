3452d9fa12a66a0f3795f475e4aedc87da647eb5
xiaolu.huo <xiaolu.huo@thoughtworks.com>
2025-05-15 11:34:08 +0800

    feat: init car-builtin-lib

--- car-builtin-lib/build.gradle.kts
+++ car-builtin-lib/build.gradle.kts
@@ -13,7 +13,33 @@ kotlin {
 }
 
 dependencies {
-    compileOnly(files("libs/framework.jar"))
-    compileOnly(files("libs/framework-bluetooth.jar"))
+    compileOnly(files("${project.projectDir}/libs/framework.jar"))
+    compileOnly(files("${project.projectDir}/libs/framework-bluetooth.jar"))
+    compileOnly(files("${project.projectDir}/libs/core-libart.jar"))
+
 }
+
+tasks.getByName("compileJava").dependsOn("generateEventLogTags")
+
+/**
+ * Generate EventLogTags when build
+ * Usage: java-event-log-tags.py [-o output_file] <input_file> <merged_tags_file>
+ */
+tasks.create<Exec>("generateEventLogTags") {
+    description = "Generate EventLogTags java class by python3."
+    group = "Build"
+
+    executable = "python3"
+    args(
+        "${rootProject.projectDir}/scripts/java-event-log-tags.py",
+        "-o",
+        "${project.projectDir}/src/main/java/android/car/builtin/util/EventLogTags.java",
+        "${project.projectDir}/src/main/java/android/car/builtin/util/EventLogTags.logtags"
+    )
+
+    doLast {
+        println("Generated EventLogTags successfully")
+    }
+}
+
 
