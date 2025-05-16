2e53cad58099aeddcee6c6441b704798ea9debe4
Yidi Zhao <yidi.zhao@thoughtworks.com>
2025-05-16 17:20:05 +0800

    feat: add dependencies

--- car-service/build.gradle.kts
+++ car-service/build.gradle.kts
@@ -32,8 +32,38 @@ android {
     }
 }
 
-dependencies {
+afterEvaluate {
+    tasks.withType<JavaCompile>().configureEach {
+        val customFrameworkJar = rootProject.file("libs/framework.jar")
+        val customFrameworkWifiJar = rootProject.file("libs/framework-wifi.jar")
+        val customLocationJar = rootProject.file("libs/framework-location.jar")
+        val customLibs = this.project
+            .files(
+                customFrameworkJar, customFrameworkWifiJar, customLocationJar
+            )
+            .filter { it.exists() }
+        if (!customLibs.isEmpty) {
+            classpath = customLibs + classpath
+        } else {
+            println("Warning: Custom JARs not found at specified paths for task ${name}.")
+            println("Searched paths: ${customFrameworkJar.absolutePath}, ${customFrameworkWifiJar.absolutePath}")
+        }
+    }
+}
 
+dependencies {
+    compileOnly(files("${rootProject.projectDir}/libs/framework.jar"))
+    compileOnly(files("${rootProject.projectDir}/libs/framework-wifi.jar"))
+    compileOnly(files("${rootProject.projectDir}/libs/core-libart.jar"))
+    compileOnly(files("${rootProject.projectDir}/libs/android-car.jar"))
+    compileOnly(files("${rootProject.projectDir}/libs/framework-location.jar"))
+
+    compileOnly(files("${rootProject.projectDir}/libs/framework-connectivity.jar"))
+    compileOnly(files("${rootProject.projectDir}/libs/framework-connectivity-t.jar"))
+    compileOnly(files("${rootProject.projectDir}/libs/framework-statsd.jar"))
+    compileOnly(files("${rootProject.projectDir}/libs/framework-tethering.jar"))
+
+//    implementation(project(":car-lib"))
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
