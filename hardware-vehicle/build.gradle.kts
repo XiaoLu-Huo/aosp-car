874eaa9992dec6687682323f0d68f56f8bd27725
Yidi Zhao <yidi.zhao@thoughtworks.com>
2025-05-20 14:45:58 +0800

    feat: update hardware-vehicle sourceSets

--- hardware-vehicle/build.gradle.kts
+++ hardware-vehicle/build.gradle.kts
@@ -1,6 +1,9 @@
+import com.google.protobuf.gradle.proto
+
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.protobuf)
 }
 
 android {
@@ -30,11 +33,33 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+
+    sourceSets["main"].proto {
+        srcDir("src/main/java")
+    }
+
+}
+
+protobuf {
+    protoc {
+        artifact = libs.protoc.protoc.get().toString()
+    }
+    generateProtoTasks {
+        all().forEach { task ->
+            // For Android, Javalite is recommended for smaller code size.
+            task.builtins {
+                create("java") { // Use 'create' for safer configuration
+                    option("lite")
+                }
+            }
+        }
+    }
 }
 
 dependencies {
-
     api(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+
+    api(libs.protobuf.javalite)
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
